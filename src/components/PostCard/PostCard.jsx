154db77c88e25ac0a655d1201fbbc509494e4c22
Andrei <andrei_1002@hotmail.com>
2024-02-02 19:04:37 +0100

    patata

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './postCard.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./postCard.css";
 
 function PostCard(post) {
   return (
-    <div className='PostCard'>
+    <div className="PostCard">
       <Link to={`/posts/${post.id}`} key={post.id}>
         <div>
           <div>
+            <h1>holaaaaa</h1>
             <h3>{post.title}</h3>
-            <p><em>Written by</em> {post.user}</p>
+            <p>
+              <em>Written by</em> {post.user}
+            </p>
           </div>
         </div>
       </Link>
